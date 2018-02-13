fc5c759d446bc7c163dc949b445cb767f01d7078
Digus-PC\Digus <diguss@gmail.com>
2018-02-13 13:20:02 -0200

    Animal Husbandry Mod Version 2.1.0.beta.2 - Some ajustes to the episodes texts. - Better comments in the AnimalsChannel - Dependency ajust.

--- ButcherMod/animals/LivingWithTheAnimalsChannel.cs
+++ ButcherMod/animals/LivingWithTheAnimalsChannel.cs
@@ -25,55 +25,77 @@ namespace AnimalHusbandryMod.animals
             this._episodes = new Dictionary<int, Episode>();
 
             //SPRING 1
-            _episodes.Add(1, new Episode("TV.LivingWithTheAnimals.Episode.Buildings", false, false, false));
-            _episodes.Add(2, new Episode("TV.LivingWithTheAnimals.Episode.Coop", false, false, false));
-            _episodes.Add(3, new Episode("TV.LivingWithTheAnimals.Episode.Silo", false, false, false));
-            _episodes.Add(4, new Episode("TV.LivingWithTheAnimals.Episode.Barn", false, false, false));
-            _episodes.Add(5, new Episode("TV.LivingWithTheAnimals.Episode.FeedingBasket", false, false, true));
-            _episodes.Add(6, new Episode("TV.LivingWithTheAnimals.Episode.CaveCarrot", false, false, true));
-            _episodes.Add(7, new Episode("TV.LivingWithTheAnimals.Episode.MeatPrice", true, false, false));
-            _episodes.Add(8, new Episode("TV.LivingWithTheAnimals.Episode.Wheat", false, false, true));
+            /*02*/_episodes.Add(1, new Episode("TV.LivingWithTheAnimals.Episode.Buildings", false, false, false)); 
+            /*06*/_episodes.Add(2, new Episode("TV.LivingWithTheAnimals.Episode.Coop", false, false, false));
+            /*09*/_episodes.Add(3, new Episode("TV.LivingWithTheAnimals.Episode.Silo", false, false, false));
+            /*13*/_episodes.Add(4, new Episode("TV.LivingWithTheAnimals.Episode.Barn", false, false, false));
+            /*16*/_episodes.Add(5, new Episode("TV.LivingWithTheAnimals.Episode.FeedingBasket", false, false, true));
+            /*20*/_episodes.Add(6, new Episode("TV.LivingWithTheAnimals.Episode.CaveCarrot", false, false, true));
+            /*23*/_episodes.Add(7, new Episode("TV.LivingWithTheAnimals.Episode.MeatPrice", true, false, false));
+            /*27*/_episodes.Add(8, new Episode("TV.LivingWithTheAnimals.Episode.Wheat", false, false, true));
             //SUMMER 1
-            _episodes.Add(9, new Episode("TV.LivingWithTheAnimals.Episode.CatDogTreat", false, false, true));
-            _episodes.Add(10, new Episode("TV.LivingWithTheAnimals.Episode.Radish", false, false, true));
-            _episodes.Add(11, new Episode("TV.LivingWithTheAnimals.Episode.TreatQuality", false, false, true));
-            _episodes.Add(12, new Episode("TV.LivingWithTheAnimals.Episode.TreatMilk", false, false, true));
-            _episodes.Add(13, new Episode("TV.LivingWithTheAnimals.Episode.Insemination", false, true, false));
-            _episodes.Add(14, new Episode("TV.LivingWithTheAnimals.Episode.Grape", false, false, true));
-            _episodes.Add(15, new Episode("TV.LivingWithTheAnimals.Episode.MeatDishesBuffs", true, false, false));
-            _episodes.Add(16, new Episode("TV.LivingWithTheAnimals.Episode.BokChoy", false, false, true));
+            /*02*/_episodes.Add(9, new Episode("TV.LivingWithTheAnimals.Episode.CatDogTreat", false, false, true));
+            /*06*/_episodes.Add(10, new Episode("TV.LivingWithTheAnimals.Episode.Radish", false, false, true));
+            /*09*/_episodes.Add(11, new Episode("TV.LivingWithTheAnimals.Episode.TreatQuality", false, false, true));
+            /*13*/_episodes.Add(12, new Episode("TV.LivingWithTheAnimals.Episode.TreatMilk", false, false, true));
+            /*16*/_episodes.Add(13, new Episode("TV.LivingWithTheAnimals.Episode.Insemination", false, true, false));
+            /*20*/_episodes.Add(14, new Episode("TV.LivingWithTheAnimals.Episode.Grape", false, false, true));
+            /*23*/_episodes.Add(15, new Episode("TV.LivingWithTheAnimals.Episode.MeatDishesBuffs", true, false, false));
+            /*27*/_episodes.Add(16, new Episode("TV.LivingWithTheAnimals.Episode.BokChoy", false, false, true));
             //FALL 1
-            _episodes.Add(17, new Episode("TV.LivingWithTheAnimals.Episode.TreatIsNotHay", false, false, true));
-            _episodes.Add(18, new Episode("TV.LivingWithTheAnimals.Episode.TreatPrice", false, false, true));
-            _episodes.Add(19, new Episode("TV.LivingWithTheAnimals.Episode.PregnancyTime", false, true, false));
-            _episodes.Add(20, new Episode("TV.LivingWithTheAnimals.Episode.Corn ", false, false, true));
-            _episodes.Add(21, new Episode("TV.LivingWithTheAnimals.Episode.PregnancyRoom", false, true, false));
-            _episodes.Add(23, new Episode("TV.LivingWithTheAnimals.Episode.Heater", false, false, false));
-            _episodes.Add(24, new Episode("TV.LivingWithTheAnimals.Episode.ChikenCowMeat", true, false, false));
+            /*02*/_episodes.Add(17, new Episode("TV.LivingWithTheAnimals.Episode.TreatIsNotHay", false, false, true));
+            /*06*/_episodes.Add(18, new Episode("TV.LivingWithTheAnimals.Episode.TreatPrice", false, false, true));
+            /*09*/_episodes.Add(19, new Episode("TV.LivingWithTheAnimals.Episode.PregnancyTime", false, true, false));
+            /*13*/_episodes.Add(20, new Episode("TV.LivingWithTheAnimals.Episode.Corn ", false, false, true));
+            /*16*/_episodes.Add(21, new Episode("TV.LivingWithTheAnimals.Episode.PregnancyRoom", false, true, false));
+            /*20*/
+            /*23*/_episodes.Add(23, new Episode("TV.LivingWithTheAnimals.Episode.Heater", false, false, false));
+            /*27*/_episodes.Add(24, new Episode("TV.LivingWithTheAnimals.Episode.ChikenCowMeat", true, false, false));
             //WINTER 1
-            _episodes.Add(27, new Episode("TV.LivingWithTheAnimals.Episode.AnimalProductQuality", false, false, false));
-            _episodes.Add(29, new Episode("TV.LivingWithTheAnimals.Episode.RabbitPregnancy", false, true, false));
-            _episodes.Add(31, new Episode("TV.LivingWithTheAnimals.Episode.Kale", false, false, true));
-            _episodes.Add(32, new Episode("TV.LivingWithTheAnimals.Episode.Cauliflower", false, false, true));
+            /*02*/
+            /*06*/
+            /*09*/_episodes.Add(27, new Episode("TV.LivingWithTheAnimals.Episode.AnimalProductQuality", false, false, false));
+            /*13*/
+            /*16*/_episodes.Add(29, new Episode("TV.LivingWithTheAnimals.Episode.RabbitPregnancy", false, true, false));
+            /*20*/
+            /*23*/_episodes.Add(31, new Episode("TV.LivingWithTheAnimals.Episode.Kale", false, false, true));
+            /*27*/_episodes.Add(32, new Episode("TV.LivingWithTheAnimals.Episode.Cauliflower", false, false, true));
             //SPRING 2
-            _episodes.Add(33, new Episode("TV.LivingWithTheAnimals.Episode.CrystalFruit ", false, false, true));
-            _episodes.Add(35, new Episode("TV.LivingWithTheAnimals.Episode.BeanHotpot ", false, false, true));
-            _episodes.Add(39, new Episode("TV.LivingWithTheAnimals.Episode.Apple ", false, false, true));
-            _episodes.Add(40, new Episode("TV.LivingWithTheAnimals.Episode.Melon", false, false, true));
+            /*02*/_episodes.Add(33, new Episode("TV.LivingWithTheAnimals.Episode.CrystalFruit ", false, false, true));
+            /*06*/
+            /*09*/_episodes.Add(35, new Episode("TV.LivingWithTheAnimals.Episode.BeanHotpot ", false, false, true));
+            /*13*/
+            /*16*/
+            /*20*/
+            /*23*/_episodes.Add(39, new Episode("TV.LivingWithTheAnimals.Episode.Apple ", false, false, true));
+            /*27*/_episodes.Add(40, new Episode("TV.LivingWithTheAnimals.Episode.Melon", false, false, true));
             //SUMMER 2
-            _episodes.Add(43, new Episode("TV.LivingWithTheAnimals.Episode.RedCabbage ", false, false, true));
-            _episodes.Add(48, new Episode("TV.LivingWithTheAnimals.Episode.Amaranth ", false, false, true));
+            /*02*/
+            /*06*/
+            /*09*/_episodes.Add(43, new Episode("TV.LivingWithTheAnimals.Episode.RedCabbage ", false, false, true));
+            /*13*/
+            /*16*/
+            /*20*/
+            /*23*/
+            /*27*/_episodes.Add(48, new Episode("TV.LivingWithTheAnimals.Episode.Amaranth ", false, false, true));
             //FALL 2
-            _episodes.Add(49, new Episode("TV.LivingWithTheAnimals.Episode.Yam ", false, false, true));
-            _episodes.Add(51, new Episode("TV.LivingWithTheAnimals.Episode.Artichoke ", false, false, true));
-            _episodes.Add(52, new Episode("TV.LivingWithTheAnimals.Episode.Pumpkin ", false, false, true));
-            _episodes.Add(55, new Episode("TV.LivingWithTheAnimals.Episode.PigsWinterPart1", false, true, false));
-            _episodes.Add(56, new Episode("TV.LivingWithTheAnimals.Episode.PigWinterPart2", false, true, false));
+            /*02*/_episodes.Add(49, new Episode("TV.LivingWithTheAnimals.Episode.Yam ", false, false, true));
+            /*06*/
+            /*09*/_episodes.Add(51, new Episode("TV.LivingWithTheAnimals.Episode.Artichoke ", false, false, true));
+            /*13*/_episodes.Add(52, new Episode("TV.LivingWithTheAnimals.Episode.Pumpkin ", false, false, true));
+            /*16*/
+            /*20*/
+            /*23*/_episodes.Add(55, new Episode("TV.LivingWithTheAnimals.Episode.PigsWinterPart1", false, true, false));
+            /*27*/_episodes.Add(56, new Episode("TV.LivingWithTheAnimals.Episode.PigWinterPart2", false, true, false));
             // WINTER 2
-            _episodes.Add(57, new Episode("TV.LivingWithTheAnimals.Episode.PigWinterPart3", false, true, true));
-            _episodes.Add(61, new Episode("TV.LivingWithTheAnimals.Episode.PigWinterPart4", false, true, false));
-            _episodes.Add(62, new Episode("TV.LivingWithTheAnimals.Episode.PigWinterPart5", false, true, true));
-            _episodes.Add(64, new Episode("TV.LivingWithTheAnimals.Episode.Retirement", false, false, false));
+            /*02*/_episodes.Add(57, new Episode("TV.LivingWithTheAnimals.Episode.PigWinterPart3", false, true, true));
+            /*06*/
+            /*09*/
+            /*13*/
+            /*16*/_episodes.Add(61, new Episode("TV.LivingWithTheAnimals.Episode.PigWinterPart4", false, true, false));
+            /*20*/_episodes.Add(62, new Episode("TV.LivingWithTheAnimals.Episode.PigWinterPart5", false, true, true));
+            /*23*/
+            /*27*/_episodes.Add(64, new Episode("TV.LivingWithTheAnimals.Episode.Retirement", false, false, false));
         }
 
         public void CheckChannelDay()
