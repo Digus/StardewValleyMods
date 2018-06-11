3f05dd77208952c8a802987dccc694174e619f69
Digus-PC\Digus <diguss@gmail.com>
2018-06-11 20:49:40 -0300

    Water Retaining Field Mod 1.0.0 - Classes refactory. - Letter to explain the mod. - Default and portuguese i18n files.

--- WaterRetainingFieldMod/WaterRetainingFieldModEntry.cs
+++ WaterRetainingFieldMod/WaterRetainingFieldModEntry.cs
@@ -13,75 +13,20 @@ namespace WaterRetainingFieldMod
 {
     public class WaterRetainingFieldModEntry : Mod
     {
-        internal static Config config;
-
-        internal static Dictionary<Vector2, int> TileLocationState =  new Dictionary<Vector2, int>();
-
+        internal static DataLoader DataLoader;
 
         public override void Entry(IModHelper helper)
         {
-            //Monitor.Log($"Started {helper.ModRegistry.ModID} from folder: {helper.DirectoryPath}");
+            DataLoader = new DataLoader(helper);
 
-            config = helper.ReadConfig<Config>();
-
-            TimeEvents.AfterDayStarted += (x, y) => TileLocationState.Clear();
+            TimeEvents.AfterDayStarted += (x, y) => HoeDirtOverrides.TileLocationState.Clear();
 
             var harmony = HarmonyInstance.Create("Digus.WaterRetainingFieldMod");
 
             var hoeDirtDayUpdate = typeof(HoeDirt).GetMethod("dayUpdate");
-            var waterRetainingFieldModEntryDayUpdatePrefix = typeof(WaterRetainingFieldModEntry).GetMethod("DayUpdatePrefix");
-            var waterRetainingFieldModEntryDayUpdatePostfix = typeof(WaterRetainingFieldModEntry).GetMethod("DayUpdatePostfix");
-            harmony.Patch(hoeDirtDayUpdate, new HarmonyMethod(waterRetainingFieldModEntryDayUpdatePrefix), new HarmonyMethod(waterRetainingFieldModEntryDayUpdatePostfix));
-
-        }
-
-        public static bool DayUpdatePrefix(HoeDirt __instance, ref int __state)
-        {
-            __state = __instance.state.Value;
-            return true;
-        }
-
-        public static void DayUpdatePostfix(HoeDirt __instance, ref GameLocation environment, ref Vector2 tileLocation, ref int __state)
-        {
-            if (environment is Farm farm)
-            {
-                if (__state == 1 && __instance.fertilizer.Value == 370 || __instance.fertilizer.Value == 371)
-                {
-                    if (TileLocationState.ContainsKey(tileLocation))
-                    {
-                        __instance.state.Value = TileLocationState[tileLocation];
-                        return;
-                    }
-                    else
-                    {
-                        TileLocationState[tileLocation] = __instance.state.Value;
-                        AddStateAdjacentFertilizedTiles(farm, tileLocation, __instance.state.Value, __instance.fertilizer.Value);
-                    }
-                }
-            }
-        }
-
-        private static void AddStateAdjacentFertilizedTiles(Farm farm, Vector2 tileLocation, int stateValue, int fertilizer)
-        {
-            Vector2[] adjasent = new Vector2[]
-            {
-                new Vector2(tileLocation.X, tileLocation.Y + 1)
-                , new Vector2(tileLocation.X + 1, tileLocation.Y) 
-                , new Vector2(tileLocation.X - 1, tileLocation.Y) 
-                , new Vector2(tileLocation.X, tileLocation.Y - 1) 
-            };
-            foreach (var adjacentTileLocation in adjasent)
-            {
-                if (!TileLocationState.ContainsKey(adjacentTileLocation) && farm.terrainFeatures.ContainsKey(adjacentTileLocation) && farm.terrainFeatures[adjacentTileLocation] is HoeDirt hoeDirt)
-                {
-                    if (hoeDirt.state.Value == 1 && hoeDirt.fertilizer.Value == fertilizer)
-                    {
-                        TileLocationState[adjacentTileLocation] = stateValue;
-                        AddStateAdjacentFertilizedTiles(farm, adjacentTileLocation, stateValue, fertilizer);
-                    }
-                }
-            }
-            
+            var hoeDirtOverridesDayUpdatePrefix = typeof(HoeDirtOverrides).GetMethod("DayUpdatePrefix");
+            var hoeDirtOverridesDayUpdatePostfix = typeof(HoeDirtOverrides).GetMethod("DayUpdatePostfix");
+            harmony.Patch(hoeDirtDayUpdate, new HarmonyMethod(hoeDirtOverridesDayUpdatePrefix), new HarmonyMethod(hoeDirtOverridesDayUpdatePostfix));
         }
     }
 }
