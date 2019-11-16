68c6a3d37db10b3a58d662ac63856483a63cc7af
Digus-PC\Digus <diguss@gmail.com>
2019-11-16 02:07:41 -0300

    Custom Kissing Mod 1.1.0 - Properly syncing the kissing animation. - Config option to disable kissing between players.

--- CustomKissingMod/FarmerOverrides.cs
+++ CustomKissingMod/FarmerOverrides.cs
@@ -12,9 +12,17 @@ namespace CustomKissingMod
     {
         public static void checkAction(Farmer __instance, ref Farmer who, ref bool __result, GameLocation location)
         {
-            if (__result != true && who.IsLocalPlayer)
+            if (__result != true)
             {
-                if (!__instance.isMoving() && !__instance.isInBed.Value && __instance.ActiveObject == null && who.ActiveObject == null && !__instance.FarmerSprite.PauseForSingleAnimation && !who.FarmerSprite.PauseForSingleAnimation)
+                if (!__instance.isMoving() 
+                    && !__instance.isInBed.Value
+                    && !__instance.UsingTool
+                    && __instance.ActiveObject == null 
+                    && who.ActiveObject == null 
+                    && !__instance.FarmerSprite.PauseForSingleAnimation 
+                    && !who.FarmerSprite.PauseForSingleAnimation
+                    && (who.team.GetFriendship(who.UniqueMultiplayerID,__instance.UniqueMultiplayerID).IsMarried()
+                    || who.team.GetFriendship(who.UniqueMultiplayerID, __instance.UniqueMultiplayerID).IsEngaged()))
                 {
                     __instance.faceGeneralDirection(who.getStandingPosition(), 0, false);
                     who.faceGeneralDirection(__instance.getStandingPosition(), 0, false);
@@ -33,20 +41,22 @@ namespace CustomKissingMod
                                 new AnimatedSprite.endOfAnimationBehavior(Farmer.completelyStopAnimating), true)
                         }.ToArray());
 
-
-                        DataLoader.Helper.Reflection.GetField<Multiplayer>(typeof(Game1), "multiplayer").GetValue().broadcastSprites(who.currentLocation, new TemporaryAnimatedSprite[1]
+                        if (who.IsLocalPlayer)
                         {
-                            new TemporaryAnimatedSprite("LooseSprites\\Cursors",
-                                new Microsoft.Xna.Framework.Rectangle(211, 428, 7, 6), 2000f, 1, 0,
-                                new Vector2((float) __instance.getTileX(), (float) __instance.getTileY()) *
-                                64f + new Vector2(16f, -64f), false, false, 1f, 0.0f, Color.White, 4f, 0.0f,
-                                0.0f, 0.0f, false)
+                            DataLoader.Helper.Reflection.GetField<Multiplayer>(typeof(Game1), "multiplayer").GetValue().broadcastSprites(who.currentLocation, new TemporaryAnimatedSprite[1]
                             {
-                                motion = new Vector2(0.0f, -0.5f),
-                                alphaFade = 0.01f
-                            }
-                        });
-                        location.playSound("dwop");
+                                new TemporaryAnimatedSprite("LooseSprites\\Cursors",
+                                    new Microsoft.Xna.Framework.Rectangle(211, 428, 7, 6), 2000f, 1, 0,
+                                    new Vector2((float) __instance.getTileX(), (float) __instance.getTileY()) *
+                                    64f + new Vector2(16f, -64f), false, false, 1f, 0.0f, Color.White, 4f, 0.0f,
+                                    0.0f, 0.0f, false)
+                                {
+                                    motion = new Vector2(0.0f, -0.5f),
+                                    alphaFade = 0.01f
+                                }
+                            });
+                            location.playSound("dwop");
+                        }
 
                         who.CanMove = false;
                         who.FarmerSprite.PauseForSingleAnimation = false;
@@ -59,6 +69,8 @@ namespace CustomKissingMod
                                 new AnimatedSprite.endOfAnimationBehavior(Farmer.completelyStopAnimating), true)
                         }.ToArray());
                         __result = true;
+
+                        DataLoader.Helper.Multiplayer.SendMessage(new KissingMessage(who.UniqueMultiplayerID,__instance.UniqueMultiplayerID), CustomKissingModEntry.MessageType);
                     }
                 }
             }
