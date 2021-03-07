d03b2a9ddd33143c4b7111e26a73d63c49c19015
Digus-PC\Digus <diguss@gmail.com>
2021-03-07 16:47:09 -0300

    Custom Cask Mod 1.3.0 - Adding Generic Mod Config Menu - Adding Custom Ager, to make any object behave like a Cask. - Transpile of sound and animation from the Cask, to not happen for custom agers. - Change to letter property, so you can enable or disable without reloading the game. - Code to avoid Custom Ager conflicts, using the first one to be loaded. - Code to avoid adding "Cask" as a custom ager. - Adding support to colored objects being accepted by Cask.

---
+++ CustomCaskMod/CustomAger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomCaskMod
+{
+    public class CustomAger
+    {
+        public string ModUniqueID;
+        public string Name;
+        public bool EnableAgingAnywhere;
+        public bool EnableMoreThanOneQualityIncrementPerDay;
+        public Dictionary<object, float> AgingData;
+        public Dictionary<int, float> AgingDataId = new Dictionary<int, float>();
+    }
+}
