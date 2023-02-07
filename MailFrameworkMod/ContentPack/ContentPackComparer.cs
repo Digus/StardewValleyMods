458302df45ab4eb87f63020f1d47f9e1de8bc767
Digus-PC\Digus <diguss@gmail.com>
2023-02-07 01:33:04 -0300

    Mail Framework Mod 1.15.0 - Upgrade to ModBuildConfig 4.1.0 - New error handler for the letter flow, to avoid error when executing letter dynamic items and callback code. - Letters now have a method to generate MailData String. - New API for integration with Mail Framework Mod - API method to direct load a content pack from a SMAPI mod. - API method to load a letter from an ILetter object. - API method to return a ILetter from an ID. - API method to return the MailData string of a letter identified per ID.

---
+++ MailFrameworkMod/ContentPack/ContentPackComparer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using StardewModdingAPI;
+
+namespace MailFrameworkMod.ContentPack
+{
+    internal class ContentPackComparer : IEqualityComparer<IContentPack>
+    {
+        public bool Equals(IContentPack x, IContentPack y)
+        {
+            return y != null && x != null && x.Manifest.UniqueID.Equals(y.Manifest.UniqueID);
+        }
+
+        public int GetHashCode(IContentPack obj)
+        {
+            return obj.Manifest.UniqueID.GetHashCode();
+        }
+    }
+}
