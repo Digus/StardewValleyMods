6a8fb8e4e7d0775b652e1082e247ccb03af1a685
Digus-PC\Digus <diguss@gmail.com>
2023-02-08 02:21:04 -0300

    Custom Crystalarium Mod 1.4.0 - Removing MFM dependency - Adding letter using MFM API. - New property to enable to crystalarium or cloner to clone every object. - New property to set the deafult time to clone for undefined object. - Adds new properties to Config Menu. - Fix to situations where you could get a cloner or a crystalarium to duplicate items with no wait time by removing it from the ground. - Change way to create Debris, so it use the same method as the game. - Hungarian translation.

--- CustomCrystalariumMod/ModConfig.cs
+++ CustomCrystalariumMod/ModConfig.cs
@@ -6,6 +6,8 @@
         public bool GetObjectBackOnChange;
         public bool GetObjectBackImmediately;
         public bool BlockChange;
+        public bool EnableCrystalariumCloneEveryObject;
+        public int DefaultCloningTime = 5000;
         public bool OverrideContentPackGetObjectProperties;
     }
 }
---
+++ CustomCrystalariumMod/integrations/IMailFrameworkModApi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StardewModdingAPI;
+using StardewValley;
+
+namespace CustomCrystalariumMod.integrations
+{
+    public interface IMailFrameworkModApi
+    {
+        public void RegisterLetter(ILetter iLetter, Func<ILetter, bool> condition, Action<ILetter> callback = null, Func<ILetter, List<Item>> dynamicItems = null);
+    }
+}
