aa5413546dc3e4868272827809e3ac11ecd3351c
Digus-PC\Digus <diguss@gmail.com>
2024-04-09 04:32:50 -0300

    Animal Husbandry Mod 2.6.10 - Updating dependencies - Marking deprecated warning on converting code - Fix null reference when reading modData from farmAnimals - Fix bug where exchange a shorn sheep would give null reference error. - Updating to new api of GMCM

--- ButcherMod/farmer/FarmerData.cs
+++ ButcherMod/farmer/FarmerData.cs
@@ -2,6 +2,7 @@
 using AnimalHusbandryMod.animals.data;
 using System.Collections.Generic;
 using AnimalHusbandryMod.animals;
+#pragma warning disable CS0618 // Type or member is obsolete
 
 namespace AnimalHusbandryMod.farmer
 {
